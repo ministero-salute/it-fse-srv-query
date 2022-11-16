9954ad00bdbdd57ffe90db8e88dced7e4747aa4a
Andrea Prosseda <andreaprosseda@gmail.com>
2022-11-16 09:03:55 +0100

    feat(FHIR): CRUD on Server FHIR

--- src/main/java/it/finanze/sanita/fse2/ms/srvquery/service/IFHIRSRV.java
+++ src/main/java/it/finanze/sanita/fse2/ms/srvquery/service/IFHIRSRV.java
@@ -14,11 +14,32 @@ public interface IFHIRSRV {
     /**
      * Create a new FHIR resource in the FHIR server
      * @param createDTO  The DTO to create 
-     * @return Boolean  The result of the creation 
+     * @return boolean  The result of the creation 
      */
-	Boolean create(FhirPublicationDTO createDTO);
+	boolean create(FhirPublicationDTO createDTO);
 
     /**
+     * Delete a FHIR resource in the FHIR server
+     * @param identifier  The masterIdentifier of the document 
+     * @return boolean  The result of the delete
+     */
+	boolean delete(String identifier);
+    
+	/**
+     * Replace a FHIR resource in the FHIR server
+     * @param replaceDTO  The DTO to replace 
+     * @return boolean  The result of the replace 
+     */
+	boolean replace(FhirPublicationDTO replaceDTO);
+
+    /**
+     * Update a FHIR resource metadata in the FHIR server
+     * @param updateDTO  The DTO to update
+     * @return boolean  The result of the update  
+     */
+	boolean updateMetadata(FhirPublicationDTO updateDTO);
+	
+	/**
      * Translate a code into a different codeSystem 
      * 
      * @param code  The code to be translated 
@@ -34,5 +55,6 @@ public interface IFHIRSRV {
      * @param masterIdentifier  The master identifier of the document to search 
      * @return boolean  True if the document exists on ElasticSearch 
      */
-    boolean checkExist(String masterIdentifier);
+    boolean checkExists(String masterIdentifier);
+
 }
--- src/main/java/it/finanze/sanita/fse2/ms/srvquery/service/impl/FHIRSRV.java
+++ src/main/java/it/finanze/sanita/fse2/ms/srvquery/service/impl/FHIRSRV.java
@@ -3,7 +3,21 @@
  */
 package it.finanze.sanita.fse2.ms.srvquery.service.impl;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import javax.annotation.PostConstruct;
+
 import org.hl7.fhir.r4.model.Bundle;
+import org.hl7.fhir.r4.model.Composition;
+import org.hl7.fhir.r4.model.DocumentReference;
+import org.hl7.fhir.r4.model.DocumentReference.DocumentReferenceRelatesToComponent;
+import org.hl7.fhir.r4.model.DocumentReference.DocumentRelationshipType;
+import org.hl7.fhir.r4.model.IdType;
+import org.hl7.fhir.r4.model.Resource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -13,10 +27,8 @@ import it.finanze.sanita.fse2.ms.srvquery.dto.request.FhirPublicationDTO;
 import it.finanze.sanita.fse2.ms.srvquery.exceptions.BusinessException;
 import it.finanze.sanita.fse2.ms.srvquery.service.IFHIRSRV;
 import it.finanze.sanita.fse2.ms.srvquery.utility.FHIRR4Helper;
-import it.finanze.sanita.fse2.ms.srvquery.utility.ProfileUtility;
+import it.finanze.sanita.fse2.ms.srvquery.utility.ResourceRelationshipUtility;
 import lombok.extern.slf4j.Slf4j;
-
-import javax.annotation.PostConstruct;
 
 /** 
  * FHIR Service Implementation 
@@ -25,22 +37,10 @@ import javax.annotation.PostConstruct;
 @Service
 @Slf4j
 public class FHIRSRV implements IFHIRSRV {
-	
-	/** 
-	 * Profile Utility 
-	 */
-    @Autowired
-	private ProfileUtility profileUtility;
 
-    /** 
-     * FHIR Configuration 
-     */
     @Autowired
 	private FHIRCFG fhirCFG;
 
-    /** 
-     * FHIR Client 
-     */
     private FHIRClient client;
 
     @PostConstruct
@@ -49,44 +49,122 @@ public class FHIRSRV implements IFHIRSRV {
     }
 
     @Override
-    public Boolean create(final FhirPublicationDTO createDTO) {
-    	boolean out = false;
-    	try {
-    		String json = createDTO.getJsonString();
-    		Bundle bundle = FHIRR4Helper.deserializeResource(Bundle.class, json, true);
+    public boolean create(final FhirPublicationDTO createDTO) {
+    	String json = createDTO.getJsonString();
+    	Bundle bundle = deserializeBundle(json);
+    	log.debug("FHIR bundle: {}", json);
+    	return create(bundle);
+    }
 
-    		client.saveBundleWithTransaction(bundle);
-    		out = true;
-    		log.debug("FHIR bundle: {}", json);
-    	} catch(Exception e) {
-    		log.error("Error creating new resource on FHIR Server: ", e);
-    		throw new BusinessException(e);
-    	}
-    	return out;
+	@Override
+    public boolean delete(String identifier) {
+    	DocumentReference documentReference = client.getDocumentReference(identifier);
+    	Composition composition = client.getComposition(documentReference);
+    	Bundle document = client.getDocument(composition);
+    	ResourceRelationshipUtility.run(document);
+    	List<IdType> idTypes = getIdTypesToDelete(document);
+    	if (idTypes.isEmpty()) return false;
+    	return delete(idTypes);
+    }
+
+	@Override
+    public boolean replace(FhirPublicationDTO body) {
+		String identifier = body.getIdentifier();
+		Bundle bundle = deserializeBundle(body.getJsonString());
+		String previousId = getDocumentReferenceId(identifier);
+    	if (previousId == null) throw new BusinessException("DocumentReference not found");
+    	setRelatedDocumentReference(bundle, previousId);
+    	delete(identifier);
+    	create(bundle);
+    	return true;
+    }
+
+	@Override
+    public boolean updateMetadata(FhirPublicationDTO body) {
+    	String identifier = body.getIdentifier();
+    	DocumentReference documentReference = client.getDocumentReference(identifier);
+    	setMetadata(documentReference, body.getJsonString());
+    	boolean result = client.update(documentReference);
+    	return result;
     }
 
     @Override
-    public String translateCode(String code, String system, String targetSystem) {
-        String out = "";
-		try {
-            if (profileUtility.isDevProfile()) {
-                out = client.translateCode(code, system, targetSystem);
-                log.info("Code translated result: {}", out);
-            } else {
-                // TODO
-            }
-
-		} catch(Exception e) {
-			log.error("Error translating Code from FHIR Terminology Server: ", e);
-			throw new BusinessException(e);
-		}
-		return out;
-    }
-
-    @Override
-    public boolean checkExist(final String masterIdentifier) {
-        boolean isFound = client.read(masterIdentifier);
+    public boolean checkExists(final String masterIdentifier) {
+        boolean isFound = client.checkExists(masterIdentifier);
         log.info("found?: {}", isFound);
         return isFound;
     }
+    
+	@Override
+    public String translateCode(String code, String system, String targetSystem) {
+        String out = client.translateCode(code, system, targetSystem);
+        log.info("Code translated result: {}", out);
+        return out;
+    }
+
+	private Bundle deserializeBundle(String json) {
+		return FHIRR4Helper.deserializeResource(Bundle.class, json, true);		
+	}
+	
+    private boolean create(Bundle bundle) {
+    	Bundle newBundle = client.create(bundle);
+    	return newBundle != null && newBundle.hasEntry();
+	}
+
+    private String getDocumentReferenceId(String masterIdentifier) {
+    	DocumentReference documentReference = client.getDocumentReference(masterIdentifier);
+    	if (documentReference == null) return null;
+    	return documentReference.getId();
+	}
+
+    private void setMetadata(DocumentReference documentReference, String jsonString) {
+    	documentReference.getCategory().get(0).getCoding().get(0).setCode("livBasso");
+		// TODO 
+	}
+
+    private void setRelatedDocumentReference(Bundle bundle, String previousIdentifier) {
+    	bundle
+		.getEntry()
+		.stream()
+		.filter(entry -> (entry.getResource() instanceof DocumentReference))
+		.forEach(entry -> setRelatedDocumentReference(entry.getResource(), previousIdentifier));
+	}
+
+	private void setRelatedDocumentReference(Resource documentReference, String previousIdentifier) {
+		DocumentReference reference = (DocumentReference) documentReference;
+		if (reference.getRelatesTo() == null) reference.setRelatesTo(new ArrayList<>());
+		DocumentReferenceRelatesToComponent related = getRelatedDocumentReference(previousIdentifier);
+		reference.getRelatesTo().add(related);
+	}
+
+	private DocumentReferenceRelatesToComponent getRelatedDocumentReference(String previousIdentifier) {
+		DocumentReferenceRelatesToComponent related = new DocumentReferenceRelatesToComponent();
+		DocumentRelationshipType code = DocumentRelationshipType.REPLACES;
+		related.setCode(code);
+		related.setId(previousIdentifier);
+		return related;
+	}
+
+	private boolean delete(List<IdType> idTypes) {
+		return idTypes
+				.stream()
+				.map(idType -> client.deleteResource(idType))
+				.allMatch(result -> result);
+	}
+
+	private List<IdType> getIdTypesToDelete(Bundle document) {
+		if (!document.hasEntry()) return new ArrayList<>();
+		return document
+				.getEntry()
+				.stream()
+				.map(entry -> entry.getResource())
+				.filter(this::canBeDeleted)
+				.map(Resource::getIdElement)
+				.collect(Collectors.toList());
+	}
+	
+	private boolean canBeDeleted(Resource resource1) {
+		return true;
+	}
+	
 }
