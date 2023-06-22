859543fee213246a41def283a97ddd05528f8a4f
Giuseppe Baittiner <Giuseppe.Baittiner-CIC@ibm.com>
2023-06-22 11:16:20 +0200

    fix: rename field for consistency across services

--- src/main/java/it/finanze/sanita/fse2/ms/srvquery/dto/response/history/HistoryDTO.java
+++ src/main/java/it/finanze/sanita/fse2/ms/srvquery/dto/response/history/HistoryDTO.java
@@ -2,13 +2,11 @@ package it.finanze.sanita.fse2.ms.srvquery.dto.response.history;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonInclude;
-import it.finanze.sanita.fse2.ms.srvquery.enums.history.HistoryOperationEnum;
 import lombok.Value;
 
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
-import java.util.Map;
 
 import static com.fasterxml.jackson.annotation.JsonInclude.Include.NON_NULL;
 
@@ -19,15 +17,15 @@ public class HistoryDTO {
     private static final String PTT_ISO_8601 = "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'";
 
     @JsonFormat(pattern = PTT_ISO_8601)
-    Date currentTime;
+    Date timestamp;
     @JsonFormat(pattern = PTT_ISO_8601)
     Date lastUpdate;
 
     List<HistoryInsertDTO> insertions;
     List<HistoryDeleteDTO> deletions;
 
-    public HistoryDTO(Date currentTime, Date lastUpdate) {
-        this.currentTime = currentTime;
+    public HistoryDTO(Date timestamp, Date lastUpdate) {
+        this.timestamp = timestamp;
         this.lastUpdate = lastUpdate;
         this.insertions = new ArrayList<>();
         this.deletions = new ArrayList<>();
