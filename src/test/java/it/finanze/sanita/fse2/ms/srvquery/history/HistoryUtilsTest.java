f5ede5c77e912605cc83b04b0de2eb8130847e8f
Giuseppe Baittiner <Giuseppe.Baittiner-CIC@ibm.com>
2023-06-29 10:37:24 +0200

    feat: add whitelist test

--- src/test/java/it/finanze/sanita/fse2/ms/srvquery/history/HistoryUtilsTest.java
+++ src/test/java/it/finanze/sanita/fse2/ms/srvquery/history/HistoryUtilsTest.java
@@ -5,11 +5,15 @@ import it.finanze.sanita.fse2.ms.srvquery.history.base.AbstractTestResources;
 import it.finanze.sanita.fse2.ms.srvquery.history.base.TestResource;
 import it.finanze.sanita.fse2.ms.srvquery.history.crud.FhirCrudClient;
 import lombok.extern.slf4j.Slf4j;
+import org.hl7.fhir.r4.model.BaseResource;
+import org.hl7.fhir.r4.model.CodeSystem;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+
+import java.util.ArrayList;
 
 import static it.finanze.sanita.fse2.ms.srvquery.config.Constants.Profile.TEST;
 import static org.junit.jupiter.api.TestInstance.Lifecycle.PER_CLASS;
@@ -42,6 +46,15 @@ public class HistoryUtilsTest extends AbstractTestResources {
             String id = crud.createResource(res.resource());
             log.info("Insert {} as {} with id {}", res.name(), res.type().getSimpleName(), id);
         }
+    }
+
+    @Test
+    void insertWhitelistResources() {
+        CodeSystem cs = (CodeSystem) createOreTestCS();
+        cs.setConcept(new ArrayList<>());
+        TestResource ore = new TestResource("ore", cs);
+        String id = crud.createResource(cs);
+        log.info("Insert {} as {} with id {}", ore.name(), ore.type().getSimpleName(), id);
     }
 
     @Test
