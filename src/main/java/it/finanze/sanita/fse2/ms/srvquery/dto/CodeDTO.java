062dfce889b6ac91ca3c597c3c6557b1ee098eab
pierascenzi <pierascenzi@gmail.com>
2023-06-30 22:53:32 +0200

    Update CodeDTO.java

--- src/main/java/it/finanze/sanita/fse2/ms/srvquery/dto/CodeDTO.java
+++ src/main/java/it/finanze/sanita/fse2/ms/srvquery/dto/CodeDTO.java
@@ -2,9 +2,11 @@ package it.finanze.sanita.fse2.ms.srvquery.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @AllArgsConstructor
+@NoArgsConstructor
 public class CodeDTO {
 	private String code;
 	private String display;
