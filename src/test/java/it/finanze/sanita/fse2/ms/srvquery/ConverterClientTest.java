9b85df9556cfe6b83f08e62b2a4a6b0eba9da178
Simone Le Noci <72795995+SimoneNuts@users.noreply.github.com>
2023-07-04 10:31:27 +0200

    feat(test): ConverterClient, TerminologyCTL

---
+++ src/test/java/it/finanze/sanita/fse2/ms/srvquery/ConverterClientTest.java
@@ -0,0 +1,114 @@
+package it.finanze.sanita.fse2.ms.srvquery;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.io.IOException;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.client.RestTemplate;
+
+import it.finanze.sanita.fse2.ms.srvquery.client.impl.ConverterClient;
+import it.finanze.sanita.fse2.ms.srvquery.config.Constants;
+import it.finanze.sanita.fse2.ms.srvquery.config.MsUrlCFG;
+import it.finanze.sanita.fse2.ms.srvquery.dto.RequestDTO;
+import it.finanze.sanita.fse2.ms.srvquery.dto.response.ConversionResponseDTO;
+import it.finanze.sanita.fse2.ms.srvquery.enums.FormatEnum;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ActiveProfiles(Constants.Profile.TEST)
+class ConverterClientTest {
+
+	@Mock
+    private MsUrlCFG msUrlCFG;
+	
+    @Mock
+    private RestTemplate restTemplate;
+
+    @InjectMocks
+    private ConverterClient converterClient;
+
+    @BeforeEach
+    void setup() {
+        MockitoAnnotations.openMocks(this);
+        when(msUrlCFG.getMsConverterHost()).thenReturn("mock-converter");
+    }
+
+    @Test
+    void testCallConvertToFhirJson() throws IOException {
+    	
+        FormatEnum format = FormatEnum.CUSTOM_JSON;
+        RequestDTO creationInfo = new RequestDTO();
+        byte[] fileBytes = "{\"test\":\"test\"}".getBytes();
+        MockMultipartFile file = new MockMultipartFile("file", "test.json", MediaType.TEXT_PLAIN_VALUE, fileBytes);
+
+        ConversionResponseDTO expectedResponse = new ConversionResponseDTO();
+        expectedResponse.setResult("Result");
+
+        // Mock restTemplate.postForObject()
+        String url = "mock-converter/v1/metadata-resource/from/" + format.toString() + "/to-fhir-json";
+        when(restTemplate.postForObject(eq(url), any(HttpEntity.class), eq(ConversionResponseDTO.class))).thenReturn(expectedResponse);
+
+        // Perform callConvertToFhirJson()
+        ConversionResponseDTO actualResponse = converterClient.callConvertToFhirJson(format, creationInfo, file);
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.MULTIPART_FORM_DATA);
+        MultiValueMap<String, Object> body = new LinkedMultiValueMap<>();
+        body.add("creationInfo", creationInfo);
+        body.add("file", new ByteArrayResource(file.getBytes()));
+        HttpEntity<MultiValueMap<String, Object>> entity = new HttpEntity<>(body, headers);
+
+        // Check call to postForObject and assertions
+        verify(restTemplate).postForObject(url, entity, ConversionResponseDTO.class);
+        assertNotNull(actualResponse);
+        assertEquals(expectedResponse.getResult(), actualResponse.getResult());
+    }
+
+    @Test
+    void testCallConvertFromFhirJson() {
+        // Mock the necessary dependencies
+        FormatEnum format = FormatEnum.FHIR_R4_XML;
+        String oid = "123";
+        byte[] file = "{\"test\":\"test\"}".getBytes();
+        
+        ConversionResponseDTO expectedResponse = new ConversionResponseDTO();
+        expectedResponse.setResult("Result");
+        
+        // Mock restTemplate.postForObject()
+        String url = msUrlCFG.getMsConverterHost()+"/v1/metadata-resource/from-fhir-json/to/"+format.toString();
+        when(restTemplate.postForObject(eq(url), any(HttpEntity.class), eq(ConversionResponseDTO.class))).thenReturn(expectedResponse);
+
+        // Perform callConvertFromFhirJson()
+        ConversionResponseDTO actualResponse = converterClient.callConvertFromFhirJson(format, oid, file);
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.MULTIPART_FORM_DATA);
+        MultiValueMap<String, Object> body = new LinkedMultiValueMap<>();
+        body.add("file", new ByteArrayResource(file));
+        HttpEntity<MultiValueMap<String, Object>> entity = new HttpEntity<>(body, headers);
+
+        // Check call to postForObject and assertions
+        verify(restTemplate).postForObject(url, entity, ConversionResponseDTO.class);
+        assertNotNull(actualResponse);
+        assertEquals(expectedResponse.getResult(), actualResponse.getResult());
+    }
+	
+}
---
+++ src/test/java/it/finanze/sanita/fse2/ms/srvquery/TerminologyCTLTest.java
@@ -0,0 +1,96 @@
+package it.finanze.sanita.fse2.ms.srvquery;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.io.IOException;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.test.context.ActiveProfiles;
+
+import brave.Tracer;
+import it.finanze.sanita.fse2.ms.srvquery.config.Constants;
+import it.finanze.sanita.fse2.ms.srvquery.controller.impl.TerminologyCTL;
+import it.finanze.sanita.fse2.ms.srvquery.dto.RequestDTO;
+import it.finanze.sanita.fse2.ms.srvquery.dto.response.terminology.GetResponseDTO;
+import it.finanze.sanita.fse2.ms.srvquery.dto.response.terminology.UploadResponseDTO;
+import it.finanze.sanita.fse2.ms.srvquery.enums.FormatEnum;
+import it.finanze.sanita.fse2.ms.srvquery.service.ITerminologySRV;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ActiveProfiles(Constants.Profile.TEST)
+class TerminologyCTLTest {
+
+	@Mock
+    private ITerminologySRV terminologySRV;
+	
+	@Mock
+	private Tracer tracer;
+
+    @InjectMocks
+    private TerminologyCTL terminologyCTL;
+
+    @Mock
+    private MockHttpServletRequest request;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    void testUploadTerminology() throws IOException {
+        FormatEnum format = FormatEnum.CUSTOM_JSON;
+        RequestDTO creationInfo = new RequestDTO();
+        MockMultipartFile file = new MockMultipartFile("file", "terminology.json", "application/json", "{\"test\":\"test\"}".getBytes());
+
+        UploadResponseDTO expectedResponse = new UploadResponseDTO();
+        expectedResponse.setInsertedItems(1);
+
+        when(terminologySRV.uploadTerminology(format, creationInfo, file)).thenReturn(expectedResponse);
+
+        UploadResponseDTO actualResponse = terminologyCTL.uploadTerminology(format, creationInfo, file, request);
+
+        // Assertions
+        assertEquals(expectedResponse, actualResponse);
+        verify(terminologySRV, times(1)).uploadTerminology(format, creationInfo, file);
+    }
+
+    @Test
+    void testGetTerminology() {
+        String oid = "terminology_oid";
+        String version = "1.0";
+
+        GetResponseDTO expectedResponse = new GetResponseDTO();
+        expectedResponse.setPresent(true);
+        expectedResponse.setCounter(1);
+
+        when(terminologySRV.isPresent(oid, version)).thenReturn(expectedResponse);
+
+        GetResponseDTO actualResponse = terminologyCTL.getTerminology(oid, version, request);
+
+        // Assertions
+        assertEquals(expectedResponse, actualResponse);
+        verify(terminologySRV, times(1)).isPresent(oid, version);
+    }
+
+    @Test
+    void testDeleteTerminology() {
+        String idResource = "terminology_id";
+
+        terminologyCTL.deleteTerminology(idResource, request);
+
+        // Verify call to service
+        verify(terminologySRV, times(1)).deleteById(idResource);
+    }
+	
+}
