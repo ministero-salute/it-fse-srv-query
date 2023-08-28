334523bdc1f6f455e430fa0b24a31b044a37335d
Giuseppe Baittiner <Giuseppe.Baittiner-CIC@ibm.com>
2023-08-28 13:05:01 +0200

    feat: add converter for metadata res

--- src/main/java/it/finanze/sanita/fse2/ms/srvquery/dto/response/SummaryResponseDTO.java
+++ src/main/java/it/finanze/sanita/fse2/ms/srvquery/dto/response/SummaryResponseDTO.java
@@ -1,9 +1,11 @@
 package it.finanze.sanita.fse2.ms.srvquery.dto.response;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hl7.fhir.r4.model.MetadataResource;
 
 @Data
 @NoArgsConstructor
@@ -15,6 +17,11 @@ public class SummaryResponseDTO extends ResponseDTO {
 	public SummaryResponseDTO(LogTraceInfoDTO traceInfo, List<SummaryResourceDTO> summaryDetailDTO) {
 		super(traceInfo);
 		this.summaryDetailDTO = summaryDetailDTO;
+	}
+
+	public static SummaryResponseDTO fromResources(LogTraceInfoDTO info, List<MetadataResource> resources) {
+		List<SummaryResourceDTO> res = resources.stream().map(SummaryResourceDTO::fromResource).collect(Collectors.toList());
+		return new SummaryResponseDTO(info, res);
 	}
 
 }
